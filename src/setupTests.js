411e79daf8939a1feaa202a48d8ba973826f0582
María Rodríguez <lu.rzapien.92@gmail.com>
2024-10-09 11:22:55 -0600

    feat(mocks): Setting handlers and server for mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,14 @@
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom'
+
+import { beforeAll, afterEach, afterAll } from 'vitest'
+import { server } from './mocks/server'
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen())
+
+// Reset anyrequest hanlers that we may add during the tests
+// do they don't affecr other tests
+afterEach(() => server.resetHandlers())
+
+// Clean uo after the tests finished
+afterAll(() => server.close())
