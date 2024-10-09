a0aac4dd70ac905ea15c7e34b6fcad2b18cd0ce2
María Rodríguez <lu.rzapien.92@gmail.com>
2024-10-09 11:21:37 -0600

    feat(SummaryForm): Testing simple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Sundaes on Demand</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
